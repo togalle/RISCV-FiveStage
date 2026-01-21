7e1b75a36bb7d2af7f1ac79ac215218ff787fc1d
Tomas Galle <tomas.galle@ugent.be>
2026-01-21 21:59:10 +0100

    :tada: Final commit

--- src/test/resources/tests/programs/beq.s
+++ src/test/resources/tests/programs/beq.s
@@ -1,23 +1,50 @@
-# Test bgeu: branch should be taken (x1 >= x2, unsigned)
 main:
     li x1, 3
     li x2, 1
-    bgeu x1, x2, label_taken
-    li x3, 0      # Should NOT execute
+    bgeu x1, x2, label_taken_1
+    li x3, 0
     done
+    nop
+label_taken_1:
+    li x3, 1
 
-label_taken:
-    li x3, 1      # Should execute (branch taken)
-    done
-
-# Test bgeu: branch should NOT be taken (x1 < x2, unsigned)
-main2:
     li x1, 2
     li x2, 5
-    bgeu x1, x2, label_not_taken
-    li x3, 2      # Should execute (branch not taken)
+    bgeu x1, x2, label_not_taken_1
+    li x3, 2
     done
+    nop
+label_not_taken_1:
+    li x3, 3
+    done
+    nop
+    li x1, 4
+    li x2, 4
+    bgeu x1, x2, label_taken_2
+    li x3, 0
+    done
+    nop
+label_taken_2:
+    li x3, 4
 
-label_not_taken:
-    li x3, 3      # Should NOT execute
+    li x1, 1
+    li x2, 2
+    bgeu x1, x2, label_not_taken_2
+    li x3, 5
+    done
+    nop
+label_not_taken_2:
+    li x3, 6
+    done
+    nop
+    li x1, 0
+    li x2, 5
+loop:
+    addi x1, x1, 1
+    bgeu x1, x2, loop_end
+    li x3, 7
+    j loop
+
+loop_end:
+    li x3, 8
     done
--- src/test/scala/Manifest.scala
+++ src/test/scala/Manifest.scala
@@ -19,7 +19,7 @@ import LogParser._
 
 object Manifest {
 
-  val singleTest = "BTreeO3.s"
+  val singleTest = "square.s"
 
   val nopPadded = false
 
